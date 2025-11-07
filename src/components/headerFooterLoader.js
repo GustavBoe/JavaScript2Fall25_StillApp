821289c53934fd60885a1e54932236e46c81c65f
GustavBoe <gustavhugoboe@icloud.com>
2025-11-07 09:31:21 +0100

    Adding bootstrap to the header. Changing from createElement to innerHTML

--- src/components/headerFooterLoader.js
+++ src/components/headerFooterLoader.js
@@ -1,47 +1,51 @@
-import { pageHeader, profileName } from "../utils/const.js";
+import { pageHeader, userName, LOGGEDIN_PROFILE_URL } from "../utils/const.js";
 import { logOut } from "../utils/storage.js";
 
 export function createHeader() {
   const headerNavigation = document.createElement("nav");
-  headerNavigation.classList = "header-navigation ";
+  headerNavigation.id = "header-nav";
+  headerNavigation.classList = "navbar navbar-expand-lg navbar-light bg-light ";
+  const pageHeaderNav = document.getElementById("header-nav");
 
-  const headerLogo = document.createElement("h1");
-  headerLogo.textContent = "Still";
-  headerLogo.classList = "header-logo tsukimi-rounded-regular";
-  headerLogo.addEventListener("click", () => {
-    location.href = "./index.html";
-  });
-
-  const globalLink = document.createElement("a");
-  globalLink.setAttribute("href", "./index.html");
-  globalLink.textContent = "Global";
-  globalLink.classList = "header-global";
-
-  const createLink = document.createElement("a");
-  createLink.setAttribute("href", "./create.html");
-  createLink.textContent = "Create";
-  createLink.classList = "header-create";
-
-  const profileLink = document.createElement("a");
-  profileLink.setAttribute("href", `./profile.html?name=${profileName}`);
-  profileLink.textContent = "Profile";
-  profileLink.classList = "header-profile";
-
-  const loginLink = document.createElement("a");
-  loginLink.setAttribute("href", "./login.html");
-  loginLink.textContent = "Login";
-
-  const logOutButton = document.createElement("button");
-  logOutButton.textContent = "Log out";
-  logOutButton.addEventListener("click", logOut);
-  logOutButton.classList = "header-button";
-
-  headerNavigation.append(headerLogo);
-
-  if (!profileName) {
-    headerNavigation.append(loginLink);
+  if (!userName) {
+    pageHeaderNav.innerHTML = `
+    <h1>Still</h1>`;
   } else {
-    headerNavigation.append(globalLink, createLink, profileLink, logOutButton);
+    pageHeaderNav.innerHTML = `
+   <div class="container-fluid">
+          <a
+            class="navbar-brand tsukimi-rounded-regular --bs-body-bg"
+            href="./index.html"
+            >Still</a
+          >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="./index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html">Global</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./create.html">Create</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./profile.html/${userName}">Profile</a>
+              </li>
+              <li class="nav-item"> <button class=btn id="log-out-btn">Log out</button>
+            </ul>
+          </div>
+        </div>`;
   }
-  pageHeader.append(headerNavigation);
 }
