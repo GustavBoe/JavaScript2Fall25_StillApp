d0ac63a3e8358005876412577f7c8cddfa7aabc8
GustavBoe <gustavhugoboe@icloud.com>
2026-06-04 11:27:14 +0200

    Styled the header using tailwindcss.

--- src/components/headerFooterLoader.js
+++ src/components/headerFooterLoader.js
@@ -3,45 +3,50 @@ import { logOut } from "../utils/storage.js";
 
 export function createHeader() {
   const headerNavigation = document.createElement("nav");
-  headerNavigation.classList = "header-navigation ";
+  headerNavigation.classList =
+    "flex items-center justify-between mx-auto px-6 py-4 border-b";
 
   const headerLogo = document.createElement("h1");
   headerLogo.textContent = "Still";
-  headerLogo.classList = "header-logo tsukimi-rounded-regular";
+  headerLogo.className =
+    "font-tsukimi text-3xl font-medium cursor-pointer hover:opacity-70";
   headerLogo.addEventListener("click", () => {
     location.href = "./index.html";
   });
+  const linksContainer = document.createElement("div");
+  linksContainer.className = "flex items-center gap-6";
 
   const globalLink = document.createElement("a");
   globalLink.setAttribute("href", "./index.html");
   globalLink.textContent = "Global";
-  globalLink.classList = "header-global";
+  globalLink.classList = "hover:opacity-70 cursor-pointer hover:underline";
 
   const createLink = document.createElement("a");
   createLink.setAttribute("href", "./create.html");
   createLink.textContent = "Create";
-  createLink.classList = "header-create";
+  createLink.classList = "hover:opacity-70 cursor-pointer hover:underline";
 
   const profileLink = document.createElement("a");
   profileLink.setAttribute("href", `./profile.html?name=${profileName}`);
   profileLink.textContent = "Profile";
-  profileLink.classList = "header-profile";
+  profileLink.classList = "hover:opacity-70 cursor-pointer hover:underline";
 
   const loginLink = document.createElement("a");
   loginLink.setAttribute("href", "./login.html");
   loginLink.textContent = "Login";
+  loginLink.className = " hover:opacity-70 cursor-pointer hover:underline";
 
   const logOutButton = document.createElement("button");
   logOutButton.textContent = "Log out";
   logOutButton.addEventListener("click", logOut);
-  logOutButton.classList = "header-button";
-
-  headerNavigation.append(headerLogo);
+  logOutButton.classList = "hover:opacity-70 cursor-pointer hover:underline";
 
   if (!profileName) {
-    headerNavigation.append(loginLink);
+    linksContainer.append(loginLink);
   } else {
-    headerNavigation.append(globalLink, createLink, profileLink, logOutButton);
+    linksContainer.append(globalLink, createLink, profileLink, logOutButton);
   }
+  headerNavigation.append(headerLogo);
+  headerNavigation.append(linksContainer);
   pageHeader.append(headerNavigation);
 }
